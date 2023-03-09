b73a57b2ba744eb136cfb807960b6372d823eeab
asura7969 <1402357969@qq.com>
2023-03-09 21:33:18 +0800

    [test]-path extract param

--- src/path/mod.rs
+++ src/path/mod.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::ops::Add;
 use std::ptr::NonNull;
 use regex::Regex;
@@ -15,6 +16,8 @@ struct PathNode {
     regex: Regex,
     left: Option<NonNull<PathNode>>,
     right: Option<NonNull<PathNode>>,
+    level: usize,
+    tags:Vec<String>
 }
 
 
@@ -22,28 +25,30 @@ impl PathNode {
 
     // route_path: /getById/:id
     fn new(path: &str) -> PathNode {
-
-        let regex_str = path.split("/")
+        let regex_vec = path.split("/")
             .filter(|s| !s.is_empty())
             .map(|s| {
                 if s.starts_with(COLON) {
-                    "(/*)".to_string()
+                    let tag_name = s.replace(COLON, "");
+                    let capture = format!("(/)(?P<{}>((\\S)+))", tag_name);
+                    (capture, Some(tag_name))
                 } else {
-                    let es = format!("(/{})", s);
-                    es
+                    let fix_str = format!("(/{})", s);
+                    (fix_str, None)
                 }
-            }).collect::<Vec<String>>()
+            }).collect::<Vec<(String, Option<String>)>>();
+
+        let level = regex_vec.len();
+
+        let tags = regex_vec.iter()
+            .filter(|(_, o)| o.is_some())
+            .map(|(_, o)| o.clone().unwrap())
+            .collect::<Vec<String>>();
+
+        let regex_str = regex_vec.iter().map(|(s, _)| s.as_str())
+            .collect::<Vec<&str>>()
             .join("");
-        // let enumerate = path.split("/").enumerate();
-        // for(_index, s) in enumerate {
-        //     let er = if s.starts_with(COLON) {
-        //         "(/:*)"
-        //     } else {
-        //         "(/*)"
-        //     };
-        // }
         let regex_str = format!("^{}", regex_str);
-        println!("{}", regex_str);
         let regex = Regex::new(regex_str.as_str()).unwrap();
 
         PathNode {
@@ -51,21 +56,41 @@ impl PathNode {
             regex,
             left: None,
             right: None,
+            level,
+            tags
         }
     }
 
-    fn pattern(&self, path: &str) -> bool {
-        self.regex.is_match(path)
+    fn pattern(&self, path: &str, deep: usize) -> bool {
+        self.level == deep && self.regex.is_match(path)
     }
+
+    fn level(&self,) -> usize {
+        self.level
+    }
+
+    fn extract_param(&self, path: &str) -> HashMap<String, String> {
+        let mut params:HashMap<String, String> = HashMap::new();
+        let caps = &self.regex.captures(path).unwrap();
+        for tag_name in &self.tags {
+            let o = &caps[tag_name.as_str()];
+            params.insert(tag_name.clone(), o.to_string());
+        }
+
+        params
+    }
+
 }
 
 #[test]
 fn match_test() {
-    let node = PathNode::new("/getById/:id");
-    let route_path1 = "/getById/1";
-    let route_path2 = "/getByName/1";
-    let route_path3 = "/aa/getById/1";
-    assert!(&node.pattern(route_path1));
-    assert!(!&node.pattern(route_path2));
-    assert!(!&node.pattern(route_path3));
+    let node1 = PathNode::new("/getById/:id");
+    let map1 = &node1.extract_param("/getById/100");
+    assert_eq!(map1.get("id"), Some(&"100".to_string()));
+
+
+    let node2 = PathNode::new("/:name/query/:id");
+    let map2 = &node2.extract_param("/张三/query/100");
+    assert_eq!(map2.get("id"), Some(&"100".to_string()));
+    assert_eq!(map2.get("name"), Some(&"张三".to_string()));
 }
